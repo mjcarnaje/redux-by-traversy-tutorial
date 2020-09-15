20be8c4f6e80328cf7a0503d0c07dd72e24c9287
mjcarnaje <michaeljamescarnaje1@gmail.com>
2020-09-15 10:13:15 +0800

    just able to run

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Post from './components/Post';
 import PostForm from './components/PostForm';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
 function App() {
 	return (
-		<div className='App'>
-			<PostForm />
-			<hr />
-			<Post />
-		</div>
+		<Provider store={store}>
+			<div className='App'>
+				<PostForm />
+				<hr />
+				<Post />
+			</div>
+		</Provider>
 	);
 }
 
